688aa63a8763ea3794c7c4f7f4783d485a483a49
Everton Silva <everton.p.silva@outlook.com>
2023-04-15 19:21:32 -0300

    create footer component test

---
+++ src/tests/exam/components/Footer.spec.js
@@ -0,0 +1,15 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Footer from "../../../Components/Footer/Footer";
+
+describe("render Footer component", () => {
+  render(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
+  test("should text 'Voltar para o topo' exist", () => {
+    expect(screen.getByText("Voltar para o topo")).toBeInTheDocument();
+  });
+});
