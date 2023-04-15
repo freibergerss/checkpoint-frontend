de238d5c6a329647936a75916a83fb093ead48f8
Everton Silva <everton.p.silva@outlook.com>
2023-04-15 19:21:06 -0300

    improve loginForm test

--- src/tests/exam/components/LoginForm.spec.js
+++ src/tests/exam/components/LoginForm.spec.js
@@ -1,18 +1,48 @@
-import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
+import api from "../../../services";
 import LoginForm from "../../../Components/LoginForm/LoginForm";
 
-describe("LoginForm component", () => {
+jest.mock("../../../services", () => ({
+  post: jest.fn(() =>
+    Promise.resolve({
+      data: {
+        username: "testuser",
+        token: "testtoken",
+      },
+    })
+  ),
+}));
+
+describe("renders LoginForm component", () => {
   render(
     <BrowserRouter>
       <LoginForm />
     </BrowserRouter>
   );
   test("should login field exist", () => {
-    expect(screen.queryByPlaceholderText("Login"));
+    expect(screen.getByPlaceholderText("Login")).toBeInTheDocument();
   });
 
-  test("should password field exist", () => {
-    expect(screen.queryAllByPlaceholderText("Password"));
+  test("handleSubmit is called with correct arguments and performs expected actions", async () => {
+    const { getByText, getByPlaceholderText } = render(
+      <BrowserRouter>
+        <LoginForm />
+      </BrowserRouter>
+    );
+    const usernameInput = getByPlaceholderText("Login");
+    const passwordInput = getByPlaceholderText("Password");
+    const button = getByText("Send");
+
+    fireEvent.change(usernameInput, { target: { value: "testuser" } });
+    fireEvent.change(passwordInput, { target: { value: "testpass" } });
+
+    fireEvent.click(button);
+
+    expect(api.post).toHaveBeenCalledWith("/auth", {
+      username: "testuser",
+      password: "testpass",
+    });
   });
 });
