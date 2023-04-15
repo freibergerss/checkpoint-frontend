df72be814d3db3b5e63c4e814d8c1b7744266f3c
Everton Silva <everton.p.silva@outlook.com>
2023-04-15 10:58:02 -0300

    some improvements

--- src/tests/exam/Routes/Login.spec.js
+++ src/tests/exam/Routes/Login.spec.js
@@ -1,7 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import Login from '../../../Routes/Login';
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import Login from "../../../Routes/Login";
 
-test('should show login form', () => {
-  render(<Login />)
+test("should show login form", () => {
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
   expect(screen.getByText("Login")).toBeInTheDocument();
 });
