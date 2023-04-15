7be7e30f974f69ad97ee3acc733eb187a9511865
Everton Silva <everton.p.silva@outlook.com>
2023-04-15 19:20:43 -0300

    improve login component test

--- src/tests/exam/components/LoginForm.spec.js
+++ src/tests/exam/components/LoginForm.spec.js
@@ -1,12 +1,15 @@
-import { end } from "@popperjs/core";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import LoginForm from "../../../Components/LoginForm/LoginForm";
 
 describe("LoginForm component", () => {
-  render(<LoginForm />);
+  render(
+    <BrowserRouter>
+      <LoginForm />
+    </BrowserRouter>
+  );
   test("should login field exist", () => {
     expect(screen.queryByPlaceholderText("Login"));
-    end;
   });
 
   test("should password field exist", () => {
