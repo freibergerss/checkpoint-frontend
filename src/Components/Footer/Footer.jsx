b1216ba0c3a4e0ce02e7e535808df077f8e209ba
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-04-16 02:21:01 -0300

    adding dark mode at Footer page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,21 @@
 import styles from "./Footer.module.css";
+import { useContext } from "react";
+import { AuthContext } from "../../context/global-context";
 
 const Footer = () => {
   const scrollToTop = () => {
     window.scrollTo(0, 0)
   }
+
+  const { theme } = useContext(AuthContext)
+
   return (
-    <footer>
-      <div className={styles.footerWrapper}>
-        <button className={`btn btn-danger ${styles.top}`} onClick={scrollToTop}>Voltar para o topo</button>
+    <footer className={`styles.footerWrapper ${theme=== 'light' ? 'navbar-light bg-light': 'navbar-dark bg-dark'}`}>
+      <div>
+        <button className={`btn btn-danger ${styles.top} `} onClick={scrollToTop}>Voltar para o topo</button>
         {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar a class navbar-dark bg-dark ou navbar-light bg-light  */}
-        <div className={`navbar-light bg-light} ${styles.footer}`}>
+        <div className={` ${styles.footer} ${theme === 'light' ? 'navbar-light bg-light': 'navbar-dark bg-dark'}`}>
           <div className="container">
             <div className={`row`}>
               <div className="col-sm-12 col-lg-6">
@@ -18,8 +23,8 @@ const Footer = () => {
                 // está em dark mode e deverá utilizar o css correto */}
                 <img className={`${styles.dhLogo}`} src="/images/DH.png" alt='DH-logo' />
               </div>
-              <div className={`col-sm-12 col-lg-6 ${styles.icons}`}>
-                <img src="/images/ico-facebook.png" alt="ícone do facebook" className={styles.icon} />
+              <div className={`col-sm-12 col-lg-6 ${styles.icons} ${theme === 'light' ? styles.icons : styles.iconsDark}`}>
+                <img src="/images/ico-facebook.png" alt="ícone do facebook" className={`${styles.icon} `} />
                 <img src="/images/ico-instagram.png" alt="ícone do instagram" className={styles.icon} />
                 <img src="/images/ico-whatsapp.png" alt="ícone do whatsapp" className={styles.icon} />
                 <img src="/images/ico-tiktok.png" alt="ícone do tiktok" className={styles.icon} />
